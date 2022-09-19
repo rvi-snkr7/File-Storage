dc470fdb219948b8d2a4f15688698ce687ab5c7e
ravi <ravi.shankar@jarvis.consulting>
2022-09-19 16:24:10 +0530

    2nd commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def index
     @users=User.all
   end
+  def new
+    @user=User.new
+  end
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :index, status: :unprocessable_entity
+
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:name,:avatar,mobile:[])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get 'users',to:'users#index'
+  get '/users',to:'users#index'
+  post '',to:"users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
